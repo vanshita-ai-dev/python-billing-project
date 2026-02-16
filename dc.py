beb909d648a668047bc7362a6f73432345db9eff
vanshita-ai-dev <vanshidadhich7@gmail.com>
2026-02-17 00:52:20 +0530

    my first billing project code
    python function for calculating a 10 rupee discount.

---
+++ dc.py
@@ -0,0 +1,6 @@
+def apply_discount(amount):
+    new_price=amount-10
+    return new_price #ye sabse jaruri he
+#ab function call kar na he
+payment=apply_discount(100)
+print("dicount ke baad aapko dene hai:",payment)
