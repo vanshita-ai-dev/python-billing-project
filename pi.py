e5db2241de56d4349a7b32d1cb6ef74e6152e755
vanshita-ai-dev <vanshidadhich7@gmail.com>
2026-02-22 00:01:35 +0530

    Add files via upload
    added practice code

---
+++ pi.py
@@ -0,0 +1,6 @@
+pi=4.14159
+diameter=3
+radius=diameter/2
+area=pi*(radius**2)
+print("the radius of the circle is:",radius)
+print("the area of the circle is:",area)
---
+++ vb.py
@@ -0,0 +1,8 @@
+a=[1,2,3]
+b=[3,2,1]
+print("before changing:a=","b=",b)
+#swap logic
+tmp=a
+a=b
+b=tmp
+print("after changing:a=","b=",b)
