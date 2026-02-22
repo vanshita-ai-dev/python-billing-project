9a5993f910ec07a0801418ff0751046cc81d6a25
vanshita-ai-dev <vanshidadhich7@gmail.com>
2026-02-22 23:36:45 +0530

    Add files via upload
    decision making codes uploaded

---
+++ age.py
@@ -0,0 +1,5 @@
+age=int(input("enter youre age:"))
+if age>=18:
+    print("you are eligible for a driving licence!")
+else:
+    print("you're still young wait a little while.")
---
+++ marks.py
@@ -0,0 +1,7 @@
+marks=int(input("enter your marks:"))
+if marks>=30:
+    print("congratulation!you passed:",marks>=30)
+    print("congratulation!you passed")
+else:
+    print("oh no,you fail:",marks<=29)  
+    print("oh no,you fail")
