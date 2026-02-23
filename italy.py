d262d66822d336eb4ea3d8183956b6621b898f0b
vanshita-ai-dev <vanshidadhich7@gmail.com>
2026-02-23 23:27:23 +0530

    created italy.py and learned python lists

---
+++ italy.py
@@ -0,0 +1,9 @@
+# my packing list
+items=["passport","laptop","visa","trolley bag","photos"]
+#print out the entire list and see
+print("my italy list:",items)
+#add a new items
+items.append("italian dictionary")
+print("update list:",items) 
+print("first item:",items[0])
+print("last item:",items[-1])
